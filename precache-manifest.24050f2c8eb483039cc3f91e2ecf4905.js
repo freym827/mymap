8772b241e941f53bfc0aa93c48afb0bcd00ff23b
Bitter Starglow <freym827@gmail.com>
2019-01-16 13:43:12 -0500

    Updates

---
+++ precache-manifest.24050f2c8eb483039cc3f91e2ecf4905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6d010f0989f9ff6600",
+    "url": "/mymap/static/js/runtime~main.db6d010f.js"
+  },
+  {
+    "revision": "f8d2fc6289c0bbf18c72",
+    "url": "/mymap/static/js/main.f8d2fc62.chunk.js"
+  },
+  {
+    "revision": "b4d36c853e2b291447d6",
+    "url": "/mymap/static/js/1.b4d36c85.chunk.js"
+  },
+  {
+    "revision": "f8d2fc6289c0bbf18c72",
+    "url": "/mymap/static/css/main.27333bff.chunk.css"
+  },
+  {
+    "revision": "b4d36c853e2b291447d6",
+    "url": "/mymap/static/css/1.7ae37f03.chunk.css"
+  },
+  {
+    "revision": "9c182ea6a45edb30d9819cdfe35970d9",
+    "url": "/mymap/index.html"
+  }
+];
