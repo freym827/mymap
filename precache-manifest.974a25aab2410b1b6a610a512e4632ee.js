0490919e9e87641485e04f7ba38196397429843b
Bitter Starglow <freym827@gmail.com>
2019-01-18 00:43:23 -0500

    Updates

--- precache-manifest.974a25aab2410b1b6a610a512e4632ee.js
+++ precache-manifest.974a25aab2410b1b6a610a512e4632ee.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/mymap/static/js/runtime~main.db6d010f.js"
   },
   {
-    "revision": "e7daf913883aae3e4d57",
-    "url": "/mymap/static/js/main.e7daf913.chunk.js"
+    "revision": "7c50d8a163cac214c4f5",
+    "url": "/mymap/static/js/main.7c50d8a1.chunk.js"
   },
   {
     "revision": "ae007566e0eae37154fd",
     "url": "/mymap/static/js/1.ae007566.chunk.js"
   },
   {
-    "revision": "e7daf913883aae3e4d57",
-    "url": "/mymap/static/css/main.9a5f8b4f.chunk.css"
+    "revision": "7c50d8a163cac214c4f5",
+    "url": "/mymap/static/css/main.86896b41.chunk.css"
   },
   {
     "revision": "ae007566e0eae37154fd",
     "url": "/mymap/static/css/1.c33ab946.chunk.css"
   },
   {
-    "revision": "e4cb891a66e04ae723e4f39cf1b927ea",
+    "revision": "1bdc18130839f2ee4290967236da4022",
     "url": "/mymap/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mymap/precache-manifest.7ed867643675d718e455a16611a7c3fb.js"
+  "/mymap/precache-manifest.974a25aab2410b1b6a610a512e4632ee.js"
 );
 
 workbox.clientsClaim();
